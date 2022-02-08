16b2cb9b426cfd6dd238df3b6dc7b1eb39beb33c
Kersten Diers <kersten.diers@dzne.de>
2022-02-08 16:38:22 +0100

    Updated qatools_cmd.py

--- qatoolspython/qatools_cmd.py
+++ qatoolspython/qatools_cmd.py
@@ -11,11 +11,7 @@ def _qatools():
     from qatoolspython import qatoolspython
 
     # parse arguments
-    subjects_dir, output_dir, subjects, subjects_file, shape, screenshots, screenshots_html, screenshots_base, screenshots_overlay, screenshots_surf, screenshots_views, screenshots_layout, fornix, fornix_html, hypothalamus, hypothalamus_html, outlier, outlier_table, fastsurfer = qatoolspython._parse_arguments()
+    subjects_dir, output_dir, subjects, subjects_file, shape, screenshots, screenshots_html, screenshots_base, screenshots_overlay, screenshots_surf, screenshots_views, screenshots_layout, fornix, fornix_html, hypothalamus, hypothalamus_html, hippocampus, hippocampus_html, hippocampus_label, outlier, outlier_table, fastsurfer = qatoolspython._parse_arguments()
 
     # run qatools
-    qatoolspython.run_qatools(subjects_dir, output_dir, subjects, subjects_file, shape, screenshots, screenshots_html, screenshots_base, screenshots_overlay, screenshots_surf, screenshots_views, screenshots_layout, fornix, fornix_html, hypothalamus, hypothalamus_html, outlier, outlier_table, fastsurfer)
-
-
-
-
+    qatoolspython.run_qatools(subjects_dir, output_dir, subjects, subjects_file, shape, screenshots, screenshots_html, screenshots_base, screenshots_overlay, screenshots_surf, screenshots_views, screenshots_layout, fornix, fornix_html, hypothalamus, hypothalamus_html, hippocampus, hippocampus_html, hippocampus_label, outlier, outlier_table, fastsurfer)
