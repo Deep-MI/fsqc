90812d705f19d8b2178312c33f7c1d92cd1268f5
diersk <kersten.diers@dzne.de>
2023-03-14 15:59:50 +0100

    Update build files

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
+# https://setuptools.pypa.io/en/latest/userguide/pyproject_config.html
+# If compatibility with legacy builds or versions of tools that don’t support
+# certain packaging standards (e.g. PEP 517 or PEP 660), a simple setup.py
+# script can be added to your project [1] (while keeping the configuration in
+# pyproject.toml):
+
 from setuptools import setup
 
 setup()
